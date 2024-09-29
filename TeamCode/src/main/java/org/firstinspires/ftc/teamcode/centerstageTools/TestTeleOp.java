c8ba64762d2812be67c55a5c61864e0015933902
allisonandreyev <allisonmandreyev@gmail.com>
2024-09-29 18:16:34 -0400

    field centric template to modify later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstageTools/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/centerstageTools/TestTeleOp.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.centerstageTools;
 
 //import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
 
 //import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 //import org.firstinspires.ftc.teamcode.subsystems.Grabber;
 //import org.firstinspires.ftc.teamcode.subsystems.PlaneLauncher;
@@ -26,9 +29,9 @@ public class TestTeleOp extends LinearOpMode {
 //    Wrist wrist;
 //    PlaneLauncher launcher;
 
-    double leftVertControl;
-    double leftHorzControl;
-    double rotate, power;
+    double y;
+    double x;
+    double rx, power;
 
 
 
@@ -54,11 +57,21 @@ public class TestTeleOp extends LinearOpMode {
 
         waitForStart();
 
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+// Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+// Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
         while(opModeIsActive()) {
 
-            leftVertControl = -gamepad1.left_stick_y;
-            leftHorzControl = gamepad1.left_stick_x;
-            rotate = -gamepad1.right_stick_x;
+            y = -gamepad1.left_stick_y;
+            x = gamepad1.left_stick_x;
+            rx = -gamepad1.right_stick_x;
+
 
             //Robot moves slower
             if(gamepad1.right_trigger > 0.4) {
@@ -68,7 +81,26 @@ public class TestTeleOp extends LinearOpMode {
                 Drivetrain.drivePower = 0.6;
             }
 
-            drivetrain.drive(leftHorzControl, leftVertControl, rotate);
+            drivetrain.drive(x, y, rx);
+
+            // Field Centric Attempt:
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            // call a method to use these?
 
             // DRIVE METHODS
 
