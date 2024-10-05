59eedbdf7e27696cd530b029d6f4497ebb343ce5
allisonandreyev <allisonmandreyev@gmail.com>
2024-10-05 14:23:56 -0400

    working field centric & robot centric tele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/trajectoryBuild.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+
+public class trajectoryBuild extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
+                .splineTo(new Vector2d(5, 6), 0)
+                .splineTo(new Vector2d(9, -10), 0)
+                .build();
+
+        drive.followTrajectory(traj);
+
+    }
+}
