3e50adce20ef9181241f5526c5aca7a70080045f
allisonandreyev <allisonmandreyev@gmail.com>
2024-11-10 13:22:19 -0500

    vertical slides class & opmode arm class & opmode
    added vertical slides and arm to subsystems group + robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeWrist.java
@@ -3,21 +3,22 @@ package org.firstinspires.ftc.teamcode.subsystems.intake;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.Subsystem;
 
 public class IntakeWrist implements Subsystem {
 
-    CRServo wrist;
+    Servo wrist;
 
     public IntakeWrist(HardwareMap hardwareMap) {
-        wrist = hardwareMap.get(CRServo.class, "wrist");
-        wrist.setDirection(DcMotorSimple.Direction.FORWARD);
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        //wrist.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void init() {
-        wrist.setPower(0); // could be wrong
+        wrist.setPosition(0); // could be wrong
     }
 
     @Override
@@ -31,14 +32,14 @@ public class IntakeWrist implements Subsystem {
     }
 
     public void transfer() {
-        wrist.setPower(0.8);
+        wrist.setPosition(0.8);
     }
 
     public void intake(){
-        wrist.setPower(0.45);
+        wrist.setPosition(0.45);
     }
 
     public void telemetry(Telemetry telemetry){
-        telemetry.addData("wrist power", wrist.getPower());
+        telemetry.addData("wrist power", wrist.getPosition());
     }
 }
