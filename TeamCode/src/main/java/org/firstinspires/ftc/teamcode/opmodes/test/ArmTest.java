4041a0d7f6933284bc373f4916177e2fcab8022c
DanielYeum <danielyeum123@gmail.com>
2024-11-10 11:49:09 -0500

    ArmTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/ArmTest.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.opmodes.test;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.opmodes.GreenLinearOpMode;
+import org.firstinspires.ftc.teamcode.subsystems.arm.Arm;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Config
+@TeleOp(name = "arm test", group = "test")
+public class ArmTest extends GreenLinearOpMode {
+
+    DcMotorEx armRotate;
+
+    public static int position = 40;
+    double power;
+    public void runOpMode() throws InterruptedException {
+
+        //arm = new Arm(hardwareMap);
+
+        armRotate = hardwareMap.get(DcMotorEx.class, "armRotate");
+        armRotate.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armRotate.setPower(0);
+        power = 0;
+        armRotate.setDirection(DcMotorSimple.Direction.REVERSE);
+        armRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRotate.setTargetPosition(0);
+        armRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        waitForStart();
+
+
+        while(opModeIsActive()) {
+/*
+            power = -gamepad2.left_trigger;
+            if (power > .75) { power = .75; }
+            arm.setArmRotatePower(power);
+            arm.telemetry(telemetry);
+*/
+            power = -gamepad1.left_stick_y;
+            armRotate.setPower(power / 2);
+
+            telemetry.addData("Rotation power:", power);
+            telemetry.addData("Arm Position:", armRotate.getCurrentPosition());
+            telemetry.addData("gamepad1.b:", gamepad1.b);
+            telemetry.update();
+        }
+
+    }
+
+}
