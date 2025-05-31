af2615a7a4cf33f8be36a41cf2a9d1693dc6b127
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2025-05-31 15:37:54 -0400

    mpt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/MotionProfileTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/MotionProfileTest.java
@@ -17,6 +17,7 @@ import org.firstinspires.ftc.teamcode.subsystems.gamepad.StickyGamepad;
 import org.firstinspires.ftc.teamcode.subsystems.util.MotionProfile;
 @Config
 @TeleOp(name="mpt", group="test")
+
 public class MotionProfileTest extends LinearOpMode {
     public StickyGamepad StickyG1;
 
@@ -25,12 +26,9 @@ public class MotionProfileTest extends LinearOpMode {
     public MotionProfile mp;
 
     public enum State {
-        IDLE,
         NORMAL,
         SPIKE,
-        PAUSE,
         RESET
-        // Add more states as needed
     }
     private State currentState = State.NORMAL;
     private ElapsedTime profileTimer = new ElapsedTime();
@@ -45,8 +43,6 @@ public class MotionProfileTest extends LinearOpMode {
         controller.setServoPwmDisable(test.getPortNumber());
         mp = new MotionProfile(0,0,0,0);
 
-
-
         waitForStart();
 
         while(opModeIsActive()) {
@@ -54,28 +50,31 @@ public class MotionProfileTest extends LinearOpMode {
                 // Start normal profile if just entered state
                 if (profileTimer.seconds() == 0) {
                     mp = new MotionProfile(0.5, test.getPosition(), vMax, aMax).start();
-                    profileTimer.reset();
                 }
                 // Wait for profile to finish
                 if (mp.done()) {
                     currentState = State.SPIKE;
                     profileTimer.reset();
                 }
+
             } else if (currentState == State.SPIKE) {
+
                 if (profileTimer.seconds() == 0) {
                     mp = new MotionProfile(xF, test.getPosition(), vMax/3, aMax/3).start();
-                    profileTimer.reset();
                 }
+
                 if (mp.done()) {
                     currentState = State.NORMAL;
                     profileTimer.reset();
                 }
             }
+
             if(gamepad1.dpad_down){
                 mp = new MotionProfile(0, test.getPosition(), vMax, aMax).start();
                 currentState = State.NORMAL;
                 profileTimer.reset();
             }
+
             TelemetryPacket packet = new TelemetryPacket();
             packet.put("ServoPosition", test.getPosition());
             dashboard.sendTelemetryPacket(packet);
@@ -85,6 +84,7 @@ public class MotionProfileTest extends LinearOpMode {
             telemetry.addData("name", name);
             telemetry.addData("position", test.getPosition());
             telemetry.addData("state", currentState);
+            telemetry.addData("elapsed time", profileTimer);
             telemetry.update();
         }
     }
