988c4e6d5bd1bd54fc4868945e1453e57bb99f47
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2025-05-31 17:00:51 -0400

    smooth servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/SmoothServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/SmoothServo.java
@@ -1,34 +1,55 @@
 package org.firstinspires.ftc.teamcode.subsystems.util;
 
-import com.qualcomm.robotcore.hardware.Servo;
-
-public class SmoothServo {
+public class SmoothServo extends GreenServo {
     static final double defaultVmax = 3, defaultAmax = 4;
 
     MotionProfile motionProfile;
     double finalPosition, currentPosition;
     double vMax, aMax;
-    public Servo servo;
 
-    public SmoothServo() {
-        this(defaultVmax, defaultAmax);
+    public SmoothServo(String name, boolean reversed) {
+        this(name, reversed, defaultVmax, defaultAmax);
     }
 
-    public SmoothServo(double vMax, double aMax) {
+    public SmoothServo(String name, boolean reversed, double vMax, double aMax) {
+        super(name, reversed);
         this.vMax = vMax;
         this.aMax = aMax;
     }
 
+    public void init() {
+        super.init();
+    }
+
+    public void read() {
+        super.read();
+    }
+
+    public void write() {
+        try {
+            currentPosition = motionProfile.getInstantTargetPosition();
+            super.setPosition(currentPosition);
+        } catch (Exception e) {
+
+        }
+        super.write();
+    }
 
     public void setPosition(double position) {
         finalPosition = position;
-        if(motionProfile == null) currentPosition = position;
+        if (motionProfile == null) currentPosition = position;
         motionProfile = new MotionProfile(finalPosition, currentPosition, vMax, aMax).start();
     }
 
+    public void setConstraints(double vMax, double aMax) {
+        this.vMax = vMax;
+        this.aMax = aMax;
+    }
 
     public void telemetry() {
         Globals.tele.addData("Target Pos: ", finalPosition);
         Globals.tele.addData("Current Pos: ", currentPosition);
+        super.telemetry();
     }
+
 }
