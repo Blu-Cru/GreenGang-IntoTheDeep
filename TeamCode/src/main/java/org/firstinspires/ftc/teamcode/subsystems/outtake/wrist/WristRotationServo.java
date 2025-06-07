b2f14ea8ef9d1b24859dbb3f8f624b05cfacd904
Michael <michael.dong.haolin@gmail.com>
2025-06-07 16:47:33 -0400

    finished wrist rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake/wrist/WristRotationServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake/wrist/WristRotationServo.java
@@ -3,98 +3,87 @@ package org.firstinspires.ftc.teamcode.subsystems.outtake.wrist;
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.util.GreenSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.util.MotionProfile;
 
-public class WristRotationServo implements GreenSubsystem, Subsystem  {
-    double pos=0.3;
-    public static double vMax = 2, aMax = 2, xI = 0, xF = 1;
-    Servo wristRotServo;
-    public MotionProfile mp;
-    STATE state;
-    private ElapsedTime profileTimer = new ElapsedTime();
+public class WristRotationServo implements GreenSubsystem, Subsystem {
+    private Servo wristRotServo;
+    private double pos = 0.3;
 
-    enum STATE {
-        NINETY,
+    private enum STATE {
+        INIT,
         FLIPPED,
-        INIT
+        NINETY
     }
+
+    private STATE currentState = STATE.INIT;
+    private STATE targetState = STATE.INIT;
+
     public WristRotationServo(HardwareMap hardwareMap) {
         wristRotServo = hardwareMap.get(Servo.class, "claw wrist");
-        state = STATE.INIT;
-        mp = new MotionProfile(0,0,0,0);
+        wristRotServo.setPosition(pos);
     }
 
-    public void flip(){
-        profileTimer.reset();
-        if(state == STATE.INIT){
-            wristRotServo.setPosition(0.85);
-//            mp = new MotionProfile(0.85, wristRotServo.getPosition(), vMax, aMax).start();
-            if(profileTimer.seconds() > 1) {
-                state = STATE.FLIPPED;
-            }
+    public void flip() {
+        if (targetState == STATE.INIT) {
+            targetState = STATE.FLIPPED;
+        } else if (targetState == STATE.FLIPPED || targetState == STATE.NINETY) {
+            targetState = STATE.INIT;
+        }
+        else{
+            targetState = STATE.INIT;
+        }
+
+
+    }
+
+    public void turn90() {
+        if (targetState != STATE.NINETY) {
+            targetState = STATE.NINETY;
         } else {
-            wristRotServo.setPosition(0.3);
-//            mp = new MotionProfile(0.3, wristRotServo.getPosition(), vMax, aMax).start();
-            if(profileTimer.seconds()>1) {
-                state = STATE.INIT;
-            }
-        }
-
-
-    }
-
-    public void turn90(){
-        profileTimer.reset();
-        if (state != STATE.NINETY){
-            wristRotServo.setPosition(0.55);
-//            mp = new MotionProfile(0.55, wristRotServo.getPosition(), vMax, aMax).start();
-            if(profileTimer.seconds()>1) {
-                state = STATE.NINETY;
-            }
-        }
-        else {
-            wristRotServo.setPosition(0.3);
-//            mp = new MotionProfile(0.3, wristRotServo.getPosition(), vMax,aMax).start();
-            if(profileTimer.seconds()>1) {
-                state = STATE.INIT;
-            }
+            targetState = STATE.INIT;
         }
     }
 
-    public void manualRotate(double incrementation) {
-        wristRotServo.setPosition(Range.clip(pos + incrementation, 0, 1));
+    public void manualRotate(double increment) {
+        pos = Range.clip(pos + increment, 0.0, 1.0);
+        wristRotServo.setPosition(pos);
+        targetState = null;
+        currentState = null;
     }
-
 
     @Override
     public void init() {
         wristRotServo.setPosition(0.3);
+        currentState = STATE.INIT;
+        targetState = STATE.INIT;
+    }
+
+    @Override
+    public void update() {
+        if (targetState != null && currentState != targetState) {
+            switch (targetState) {
+                case INIT:
+                    wristRotServo.setPosition(0.3);
+                    break;
+                case FLIPPED:
+                    wristRotServo.setPosition(0.85);
+                    break;
+                case NINETY:
+                    wristRotServo.setPosition(0.55);
+                    break;
+            }
+            currentState = targetState;
+        }
+
+        pos = wristRotServo.getPosition();
     }
 
     @Override
     public void telemetry(Telemetry telemetry) {
-        telemetry.addData("wrist rot pos:", pos);
-        telemetry.addData("state", state);
-        telemetry.addData("timer", profileTimer.seconds());
+        telemetry.addData("Wrist Position", pos);
+        telemetry.addData("Current State", currentState);
+        telemetry.addData("Target State", targetState);
     }
-
-
-    @Override
-    public void update() {
-        pos = wristRotServo.getPosition();
-
-//        if (mp != null && !mp.done()) {
-//            double target = mp.getInstantTargetPosition();
-//            wristRotServo.setPosition(target);
-//        }
-    }
-
-//    public void setPosition(double pos){
-//        mp = new MotionProfile(this.pos, pos, 4, 4).start();
-//    }
 }
