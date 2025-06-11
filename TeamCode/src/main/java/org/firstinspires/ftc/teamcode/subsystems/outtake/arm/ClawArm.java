43b2bc56a70279eb549c94cddc15ef139d212a25
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2025-06-11 17:52:21 -0400

    im switchibg to mech

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake/arm/ClawArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/outtake/arm/ClawArm.java
@@ -1,14 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystems.outtake.arm;
 
+import static org.firstinspires.ftc.teamcode.opmodes.test.MotionProfileTest.aMax;
+import static org.firstinspires.ftc.teamcode.opmodes.test.MotionProfileTest.vMax;
+
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.util.GreenSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.util.MotionProfile;
 
 public class ClawArm implements GreenSubsystem, Subsystem {
     public Servo clawArm;
+    MotionProfile mp;
+
     public enum STATE {
         INIT,
         PERP,
@@ -27,20 +33,22 @@ public class ClawArm implements GreenSubsystem, Subsystem {
     }
 
     public void intake() { // Transfer
-        clawArm.setPosition(.9); //0.08
+        mp = new MotionProfile(.9, clawArm.getPosition(), vMax*4, aMax*4).start();
         state = STATE.INIT;
     }
     public void perpendicular() { // scoring samples
-        clawArm.setPosition(.4);
+        mp = new MotionProfile(.4, clawArm.getPosition(), vMax*4, aMax*4).start();
         state = STATE.PERP;
     }
     public void vert(){ //scoring specimen
+
+        mp = new MotionProfile(.4, clawArm.getPosition(), vMax*4, aMax*4).start();
         clawArm.setPosition(.4);
         state = STATE.OUTSPEC;
     }
 
     public void inspec() {
-        clawArm.setPosition(.85); //pos + 0.38
+        mp = new MotionProfile(.85, clawArm.getPosition(), vMax*4, aMax*4).start();
         state = STATE.INSPEC;
     }
 
