2447fb4f3c1591630506b3f3ad6380e6d161a680
Allison Andreyev <137202319+allisonandreyev@users.noreply.github.com>
2025-06-15 14:08:07 -0400

    spec path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -26,25 +26,25 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setDimensions(13.964567, 15.43553)
 
-                .setConstraints(80, 80, Math.toRadians(225), Math.toRadians(225), 24)
-                .setStartPose(new Pose2d(0, 64, Math.toRadians(90)))
+                .setConstraints(80, 80, 5/4.0*Math.PI, 5/4.0*Math.PI, 24)
+                .setStartPose(new Pose2d(0, 64, Math.PI/2))
 
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0,64,Math.toRadians(90))).setTangent(Math.toRadians(-90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0,64,Math.PI/2)).setTangent(-Math.PI/2)
 
-                    .setTangent(Math.toRadians(-90))
+                    .setTangent(-Math.PI/2)
 
-                    .splineToConstantHeading(new Vector2d(-5, 36.5 - 3), Math.toRadians(-90))
+                    .splineToConstantHeading(new Vector2d(-5, 36.5 - 3), -Math.PI/2)
 
                     .waitSeconds(0.5)
-                    .setTangent(Math.toRadians(90))
+                    .setTangent(Math.PI/2)
 
                     .splineToConstantHeading(new Vector2d(-33, 37), Math.toRadians(180))
                     .splineToConstantHeading(new Vector2d(-40, 17), Math.toRadians(180))
                     .splineToConstantHeading(new Vector2d(-47, 17), Math.toRadians(180))
-                        .setTangent(90)
+                        .setTangent(Math.PI/2)
                     .splineToConstantHeading(new Vector2d(-47, 51), Math.toRadians(90))
                     .splineToConstantHeading(new Vector2d(-47, 17), Math.toRadians(90))
-                        .setTangent(180)
+                        .setTangent(Math.PI)
                     .splineToConstantHeading(new Vector2d(-58, 17), Math.toRadians(90))
 
                     .splineToConstantHeading(new Vector2d(-58, 57.3), Math.toRadians(90))
@@ -52,32 +52,32 @@ public class MeepMeepTesting {
                     .splineToConstantHeading(new Vector2d(-62, 17), Math.toRadians(90))
                     .splineToConstantHeading(new Vector2d(-62, 57.3), Math.toRadians(90))
                     .waitSeconds(1.1)
-                    .setTangent(Math.toRadians(-45))
+                    .setTangent(-Math.PI/4)
 
                     .splineToConstantHeading(new Vector2d(-6, 38 - 3), Math.toRadians(-90))
                     .waitSeconds(0.5)
-                    .setTangent(Math.toRadians(135))
+                    .setTangent(3/4.0*Math.PI)
 
                     .splineToConstantHeading(new Vector2d(-48, 56), Math.toRadians(90))
 
                     .splineToConstantHeading(new Vector2d(-48, 60.3), Math.toRadians(90))
                     .waitSeconds(1.1)
 
-                    .setTangent(Math.toRadians(-45))
+                    .setTangent(-Math.PI/4)
                     .splineToConstantHeading(new Vector2d(-7, 38.5 - 3), Math.toRadians(-90))
                     .waitSeconds(0.3)
 
-                    .setTangent(Math.toRadians(135))
+                    .setTangent(3/4.0*Math.PI)
 
                     .splineToConstantHeading(new Vector2d(-48, 56), Math.toRadians(90))
                     .splineToConstantHeading(new Vector2d(-48, 62.5), Math.toRadians(90))
                     .waitSeconds(1.1)
 
-                    .setTangent(Math.toRadians(-45))
+                    .setTangent(-Math.PI/4)
                     .splineToConstantHeading(new Vector2d(-8, 40 - 3), Math.toRadians(-90))
                     .waitSeconds(0.3)
 
-                    .setTangent(Math.toRadians(135))
+                    .setTangent(3/4.0*Math.PI)
                     .splineToConstantHeading(new Vector2d(-52, 60), Math.toRadians(90))
                     .build());
 
